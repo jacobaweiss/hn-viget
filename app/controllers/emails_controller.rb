ae77b4abd6f2a8d5ef615597890437e8de02dc75
Jack <jacobaweiss@gmail.com>
2012-06-18 17:03:55 -0400

    set up password changing and resetting via action mailer

---
+++ app/controllers/emails_controller.rb
@@ -0,0 +1,17 @@
+class EmailsController < ApplicationController
+  def new
+  end
+  
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user
+      UserMailer.reset_password(@user).deliver
+      flash[:notice] = "Your new password has been sent to your email!"
+      redirect_to "/"
+    else
+      flash[:error] = "We could not find that email address on our servers."
+      redirect_to "/reset_password"
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Hn::Application.routes.draw do
   resources :articles
-  resources :users, :only => [:new, :create, :show]
+  resources :users, :except => [:destroy, :edit]
   resources :sessions, :only => [:new, :create, :destroy]
+  resource :emails
 
   root :to => 'articles#index'
-  match '/login',  :to => 'sessions#new'
-  match '/logout', :to => 'sessions#destroy', via: :delete
-  match '/signup', :to => 'users#new'
+  match '/login',           :to => 'sessions#new'
+  match '/logout',          :to => 'sessions#destroy', via: :delete
+  match '/signup',          :to => 'users#new'
+  match '/change_password', :to => 'users#password'
+  match '/reset_password',  :to => 'emails#new'
 end
