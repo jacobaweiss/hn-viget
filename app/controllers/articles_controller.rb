54c07c075c183d1491f32b49a729c7a275412260
Jack <jacobaweiss@gmail.com>
2012-06-18 10:33:34 -0400

    deleted excess content

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,81 +2,48 @@ class ArticlesController < ApplicationController
   before_filter :require_login, :except => [:index, :show]
   before_filter :load_article, :only => [:edit, :update, :destroy]
   
-  # GET /articles
-  # GET /articles.json
   def index
     @articles = Article.chronological.page(params[:page]).per(20)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @articles }
-    end
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
   def show
     @article = Article.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @article }
-    end
   end
 
-  # GET /articles/new
-  # GET /articles/new.json
   def new
     @article = Article.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @article }
-    end
   end
 
-  # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
   def create
     @article = self.current_user.articles.create(params[:article])
 
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render json: @article, status: :created, location: @article }
       else
         format.html { render action: "new" }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PUT /articles/1
-  # PUT /articles/1.json
   def update
     respond_to do |format|
       if @article.update_attributes(params[:article])
         format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
   def destroy
     @article.destroy
 
     respond_to do |format|
       format.html { redirect_to articles_url }
-      format.json { head :no_content }
     end
   end
   
