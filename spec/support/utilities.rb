d60cf72d07e6a37481559559d0161ae79ee8c8db
Jack <jacobaweiss@gmail.com>
2012-06-19 14:32:28 -0400

    removed commented out lines

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,4 @@
 def log_in
-  # @user = User.create!(:email => 'gob@bluth.org', :password => 'tricks', :password_confirmation => 'tricks')    
   visit '/login'
   fill_in 'email', :with => 'lindsey@bluth.org'
   fill_in 'password', :with => 'beads'
