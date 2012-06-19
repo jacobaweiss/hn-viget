431ec5d8ba08b1784098987065a9bff5b59ceec3
Jack <jacobaweiss@gmail.com>
2012-06-19 15:26:47 -0400

    renamed emails to password resets

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Hn::Application.routes.draw do
   resources :articles
   resources :users, :except => [:destroy, :edit]
   resources :sessions, :only => [:new, :create, :destroy]
-  resource :emails
+  resource :password_resets
 
   root :to => 'articles#index'
   match '/login',           :to => 'sessions#new'
   match '/logout',          :to => 'sessions#destroy', via: :delete
   match '/signup',          :to => 'users#new'
   match '/change_password', :to => 'users#password'
-  match '/reset_password',  :to => 'emails#new'
+  match '/reset_password',  :to => 'password_resets#new'
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,11 +1,11 @@
 require "spec_helper"
 
 describe UserMailer do
-  let!(:user) { Factory.build(:user) }
+  let!(:user) { Factory.build(:user, :email => "george@bluth.org") }
   before(:all) { @email = UserMailer.reset_password(user) }
   
   it "should deliver the email when requested" do
-    @email.should deliver_to "lindsey@bluth.org"
+    @email.should deliver_to "george@bluth.org"
   end
   
   it "should have the right content" do
