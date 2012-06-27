5b48ef74e319731cc5c487d613118bb19ed302e5
Jack <jacobaweiss@gmail.com>
2012-06-27 09:52:30 -0400

    added precompile to production, to fix bootstrap on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Hn::Application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
   config.assets.compress = true
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
