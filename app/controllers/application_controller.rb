c03ae9d4c88524ee441e062cc53fe573fc238c89
Jack <jacobaweiss@gmail.com>
2012-06-29 10:49:35 -0400

    guests redirected to login page when clicking vote arrows, set some 404 error stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from ActiveRecord::RecordNotFound, :with => :rescue_not_found
   include SimplestAuth::Controller
+  
+  protected
+  
+  def rescue_not_found
+    render :file => "public/404.html", :status => 404
+  end
   
   private
   
   def require_login
     unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to login_url
+      respond_to do |format|
+        format.html { redirect_to login_url, :error => "You must be logged in to access this section" }
+        format.js
+      end
     end
   end
+  
 end
